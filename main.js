874da488714db7414da50aafd942849562c239ea
AlisaCosmos <57442445+AlisaCosmos@users.noreply.github.com>
2021-07-30 14:20:34 +0300

    Added slider

---
+++ main.js
@@ -0,0 +1,90 @@
+/* checkbox */
+$( function() {
+  $( "input" ).checkboxradio({
+    icon: false
+  });
+} );
+
+
+
+$(document).ready(function() {
+
+$('#radio-1').on('click',function(){
+
+        /* slader text */
+$(function () {
+    function hexFromRGB(r, g, b) {
+        var hex = [
+            r.toString(16),
+            g.toString(16),
+            b.toString(16)
+        ];
+        $.each(hex, function (nr, val) {
+            if (val.length === 1) {
+                hex[nr] = "0" + val;
+            }
+        });
+        return hex.join("").toUpperCase();
+    }
+    function refreshSwatch() {
+        var red = $("#red").slider("value"),
+            green = $("#green").slider("value"),
+            blue = $("#blue").slider("value"),
+            hex = hexFromRGB(red, green, blue);
+        $("#swatchText").css("color", "#" + hex);
+    }
+
+    $("#red, #green, #blue").slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+    $("#red").slider("value", 255);
+    $("#green").slider("value", 140);
+    $("#blue").slider("value", 60);
+});
+    })
+
+$('#radio-2').on('click',function(){
+        /* slader block*/
+$(function () {
+    function hexFromRGB(r, g, b) {
+        var hex = [
+            r.toString(16),
+            g.toString(16),
+            b.toString(16)
+        ];
+        $.each(hex, function (nr, val) {
+            if (val.length === 1) {
+                hex[nr] = "0" + val;
+            }
+        });
+        return hex.join("").toUpperCase();
+    }
+    function refreshSwatch() {
+        var red = $("#red").slider("value"),
+            green = $("#green").slider("value"),
+            blue = $("#blue").slider("value"),
+            hex = hexFromRGB(red, green, blue);
+        $("#swatch").css("background-color", "#" + hex);
+    }
+
+    $("#red, #green, #blue").slider({
+        orientation: "horizontal",
+        range: "min",
+        max: 255,
+        value: 127,
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+    $("#red").slider("value", 255);
+    $("#green").slider("value", 140);
+    $("#blue").slider("value", 60);
+});
+    })
+
+
+});
